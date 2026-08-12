328cc1d39dce2574c349b59d13d2d3449fa7720c
Huncho2030 <morinyoparks@gmail.com>
2026-08-12 12:51:45 +0300

    feat: design AppConfig database schema template using Mongoose models

---
+++ backend/models/AppConfig.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const appConfigSchema = new.mongoose.Schema({
+  isMaintenance: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+},{
+  timestamps: true
+});
+
+module.exports = mongoose.model('AppConfig', appConfigSchema);
