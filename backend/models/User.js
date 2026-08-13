92afaee8a94606ea5e8633ca9b773b4a53f9a29b
Huncho2030 <morinyoparks@gmail.com>
2026-08-13 23:11:18 +0300

    feat: design scalable User daya schema blueprint template using Mongoose models

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name "],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please provide an email address"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please provide a secure password"],
+  },
+
+  creatdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
